2389c74b0b80b773116e821a4e6ff50a8c4f47d8
shadstoneofficial <contact@shadstone.com>
2026-05-17 17:22:23 +0700

    Fix CI test entropy shim

--- unit.js
+++ unit.js
@@ -1,7 +1,28 @@
-import { configure } from 'enzyme';
-import Adapter from '@cfaester/enzyme-adapter-react-18';
+const nodeCrypto = eval('require')('crypto');
+const { configure } = require('enzyme');
+const Adapter = require('@cfaester/enzyme-adapter-react-18');
 
-configure({ adapter: new Adapter() });
+const cryptoShim = {
+  getRandomValues(array) {
+    return nodeCrypto.randomFillSync(array);
+  },
+};
+
+for (const target of [globalThis, globalThis.window, globalThis.self]) {
+  if (target && !target.crypto) {
+    Object.defineProperty(target, 'crypto', {
+      configurable: true,
+      value: cryptoShim,
+    });
+  } else if (target && !target.crypto.getRandomValues) {
+    Object.defineProperty(target.crypto, 'getRandomValues', {
+      configurable: true,
+      value: cryptoShim.getRandomValues,
+    });
+  }
+}
+
+configure({ adapter: new Adapter.default() });
 
 require('./app/pages/Auction/tests/RepairBid.spec');
 require('./app/pages/MyDomain/tests/Records.spec');
